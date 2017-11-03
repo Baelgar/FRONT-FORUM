674ddf1ec3a0e253796a015bb7aeda090df6b90a
Baelgar <enfoux.kevin@gmail.com>
2017-11-03 16:40:14 +0100

    debut vue.js

--- data/home.php
+++ data/home.php
@@ -5,6 +5,7 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/vue"></script>
 </header>
 
 
@@ -128,4 +129,20 @@ $('#latestTopics a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+
+
+$(document).ready(function(){
+    var app = new Vue({
+        el: '#app',
+        data: {
+          message: 'Hello Vue!'
+        }
+    });
+});
+
+
+</script>
+
+
+
 </script>
--- data/topicList.php
+++ data/topicList.php
@@ -5,7 +5,7 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-  <script src="node_module/vue"></script>
+  <script src="https://unpkg.com/vue"></script>
 </header>
 
 
@@ -55,16 +55,25 @@
   </div>
 
   <div class="container container-fluid" id="topic-list">
-    <div class="topic"><a href="#">TOPIC 1</a></div>
-    <div class="topic"><a href="#">TOPIC 2</a></div>
-    <div class="topic"><a href="#">TOPIC 3</a></div>
-    <div class="topic"><a href="#">TOPIC 4</a></div>
-    <div class="topic"><a href="#">TOPIC 5</a></div>
-    <div class="topic"><a href="#">TOPIC 6</a></div>
-    <div class="topic"><a href="#">TOPIC 7</a></div>
+
+    <div class="topic" id="app"><a href="#">{{ message }}</a></div>
+    <div class="topic" id="app"><a href="#">{{ message }}</a></div>
+    <div class="topic" id="app"><a href="#">{{ message }}</a></div>
+    <div class="topic" id="app"><a href="#">{{ message }}</a></div>
+    <div class="topic" id="app"><a href="#">{{ message }}</a></div>
+    <div class="topic" id="app"><a href="#">{{ message }}</a></div>
+    <div class="topic" id="app"><a href="#">{{ message }}</a></div>
   </div>
 
 </body>
+
+
+
+
+
+
+
+
 
 
 
